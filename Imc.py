399bf73dcfe6a3827150ceea35a12c9fc5491d05
WorkMauro <104741524+WorkMauro@users.noreply.github.com>
2022-09-03 01:16:38 -0300

    Update Imc.py

--- Imc.py
+++ Imc.py
@@ -1,15 +1,15 @@
-peso = 73
-altura = 1.72
+peso = float(input("Qual seu peso?: "))
+altura = float(input("Qual sua altura?: "))
 
 imc = peso / (altura * altura)
 print(imc)
 if imc <= 18.5:
-	print("Você esta abaixo do peso")
+    print("Você esta abaixo do peso")
 elif imc <= 24.9:
-	print("Este é o Peso ideal")
+    print("Este é o Peso ideal")
 elif imc <= 29.9:
-	print("Você esta acima do peso obesidade grau 1")
+    print("Você esta acima do peso obesidade grau 1")
 elif imc <= 34.9:
-	print("Voce esta acima do peso obesidade grau 2")
+    print("Voce esta acima do peso obesidade grau 2")
 elif imc >= 40:
-	print("você esta acima do peso obesidade grau 3")
+    print("você esta acima do peso obesidade grau 3")
